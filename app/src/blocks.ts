3a4572c9d70505dfa8fae1f03b064b9663c14b57
Kamil Jamróz <nergydev@gmail.com>
2020-04-11 13:59:32 +0200

    webgl types installed, using ts types in react and webgl, skeleton done

---
+++ app/src/blocks.ts
@@ -0,0 +1,70 @@
+//import './main.scss';
+
+//class Socket {
+    //block1, block2
+//}
+
+/*
+class Block {
+    constructor() {
+
+    }
+}
+
+class AddBlock extends Block {
+    inputValueA: number = 0;
+    inputValueB: number = 0;
+    outputValue: number;
+
+    inputBlockA: Block;
+    inputBlockB: Block;
+    outputBlock: Block;
+
+    outputType: "number";
+
+    inputAIsConnected: boolean;
+    inputBIsConnected: boolean;
+
+    OnConnectTry() {
+        let result = this.Validate();
+    }
+
+    Validate(): boolean {
+        //if (outputBlock.)
+
+        return true;
+    }
+
+    UpdateInputs() {
+        if (this.inputBlockAConnected)
+            this.inputValueA = this.inputBlockA.outputValue;
+         
+        if (this.inputBlockBConnected)
+            this.inputValueB = this.inputBlockB.outputValue;
+    }
+
+    Update() {
+        this.UpdateInputs();
+        this.outputValue = this.inputValueA + this.inputValueB;
+        this.outputBlock.Update();
+    }
+}
+
+function App() {
+
+    let block1 = new AddBlock();
+    let block2 = new AddBlock();
+
+    let success: boolean = TryToMakeConnection(block1.outputs[0], block2.inputs[0]);
+    if (!success) {
+        alert("error");
+    }
+
+    block1.Update();
+    console.log("result: " + block1.outputValue);
+
+    while (true) {
+        //firstBlock.Update();
+    }
+}
+*/
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import App from './components/App';
+//import * as index from './index';
+//index.default.x
 
 ReactDOM.render (
 <App color="Blue" />,
