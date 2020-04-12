337f6f3359f1b6c6dd67f500eacf415ce42522e5
Kamil Jamróz <nergydev@gmail.com>
2020-04-12 20:16:40 +0200

    removed some files

--- app/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./main.scss");
-const hello = "yep, it's working";
-document.body.innerHTML = hello;
-//# sourceMappingURL=index.js.map
