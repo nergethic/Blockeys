d8ff1e00371346740fc593d6ae7ec291c33b7351
Kamil Jamróz <nergydev@gmail.com>
2020-06-11 22:36:20 +0200

    cleanup

--- app/src/components/BlockGrid.tsx
+++ app/src/components/BlockGrid.tsx
@@ -433,7 +433,7 @@ class SVG extends React.Component {
             reset
         }: SVGProps = this.props as SVGProps
 
-        let circles = blocks.map((block: GridBlockContainer, i: any, a: any) => {
+        let SVGblocks = blocks.map((block: GridBlockContainer, i: any, a: any) => {
             let pos = block.gridPosition; // todo
             let anchors = []
 
@@ -498,51 +498,6 @@ class SVG extends React.Component {
                     {table}
                 </g>
             )
-            
-            /*
-            if (p.q) {
-                anchors.push(
-                    <Quadratic
-                        index={ i }
-                        p1x={ a[i - 1].x }
-                        p1y={ a[i - 1].y }
-                        p2x={ p.x }
-                        p2y={ p.y }
-                        x={ p.q.x }
-                        y={ p.q.y }
-                        setDraggedQuadratic={ setDraggedQuadratic } />
-                )
-            } else if (p.c) {
-                anchors.push(
-                    <Cubic
-                        index={ i }
-                        p1x={ a[i - 1].x }
-                        p1y={ a[i - 1].y }
-                        p2x={ p.x }
-                        p2y={ p.y }
-                        x1={ p.c[0].x }
-                        y1={ p.c[0].y }
-                        x2={ p.c[1].x }
-                        y2={ p.c[1].y }
-                        setDraggedCubic={ setDraggedCubic } />
-                )
-            }
-            
-            return (
-                <g className={
-                    "ad-PointGroup" +
-                    (i === 0 ? "  ad-PointGroup--first" : "") +
-                    (activeBlockIndex === i ? "  is-active" : "")
-                }>
-                    <Point
-                        index={ i }
-                        x={ p.x }
-                        y={ p.y }
-                        setDraggedPoint={ setDraggedPoint } />
-                    { anchors }
-                </g>
-            )
-            */
         })
 
         return (
@@ -560,7 +515,7 @@ class SVG extends React.Component {
                     className="ad-Path"
                     d={ path } />
                 <g className="ad-Points">
-                    { circles }
+                    { SVGblocks }
                 </g>
             </svg>
         )
